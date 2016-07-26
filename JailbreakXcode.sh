400f83f54303c68d85a64d56fe27e9505b041d3c
Pr0gmaT1k <azerty@pr0gmat1k.com>
2016-07-26 16:56:29 +0200

    add admin privilege

--- JailbreakXcode.sh
+++ JailbreakXcode.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-git clone https://github.com/AzerTyQsdF/PrivateFrameworks /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/PrivateFrameworks
-rm /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/SDKSettings.plist
-git clone /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/temp
-mv /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/temp/SDKSettings.plist ..
-rm -r /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/temp
+
+# clone PrivateFrameworks
+sudo git clone https://github.com/AzerTyQsdF/PrivateFrameworks /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/PrivateFrameworks
+
+# Remove existing SDKSettings
+sudo rm /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/SDKSettings.plist
+
+# Clone new SDKSettings in temp dir with ADHoc sign enable
+sudo git clone /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/temp
+
+# Move new SDKSettings at the correct place
+sudo mv /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/temp/SDKSettings.plist ..
+
+# Remove Temp dir.
+sudo rm -r /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/temp
+
 echo "done"
