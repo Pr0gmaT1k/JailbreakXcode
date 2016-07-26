d45d6a3857e4e3dea2a2d13290b2d8f2c84d3933
Pr0gmaT1k <azerty@pr0gmat1k.com>
2016-07-26 17:17:38 +0200

    Existing Private Frameworks check
    Remove private frameworks if exist.

--- JailbreakXcode.sh
+++ JailbreakXcode.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -d "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/PrivateFrameworks" ]
+then
+	echo "remove existing PrivateFrameworks"
+  sudo rm -r /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/PrivateFrameworks
+
 # clone PrivateFrameworks
 sudo git clone https://github.com/AzerTyQsdF/PrivateFrameworks /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/PrivateFrameworks
 
@@ -7,7 +12,7 @@ sudo git clone https://github.com/AzerTyQsdF/PrivateFrameworks /Applications/Xco
 sudo rm /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/SDKSettings.plist
 
 # Clone new SDKSettings in temp dir with ADHoc sign enable
-sudo git clone /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/temp
+sudo git clone https://github.com/AzerTyQsdF/SDKSettingsJailbreak /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/temp
 
 # Move new SDKSettings at the correct place
 sudo mv /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/temp/SDKSettings.plist ..
